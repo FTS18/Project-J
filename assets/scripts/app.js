10c2c0139c3f7697ea7259cff51cd84da34dae56
Ananay <73402363+FTS18@users.noreply.github.com>
2025-02-26 12:13:32 +0530

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rankInput = document.getElementById("marks");
     const percentileInput = document.getElementById("perc");
-    const totalCandidates = 1475000; 
+    const totalCandidates = 1520000; 
     const categoryInputs = document.querySelectorAll('input[name="category"]');
     const genderInputs = document.querySelectorAll('input[name="gender"]');
     
